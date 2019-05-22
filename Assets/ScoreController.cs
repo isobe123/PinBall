0814fe1d3a5a0536c75b8b6eda5bcda5bb302a86
isobe <ngnp.pngn@icloud.com>
2019-05-22 17:05:05 +0900

    PinBallプロジェクト
    Lesson5 3Dゲーム・得点表示

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+
+	private GameObject scoreText;
+	private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+		this.scoreText = GameObject.Find ("ScoreText");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (this.score >= 0) {
+			this.scoreText.GetComponent<Text> ().text = ("Score" + this.score);
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+		
+
+		if (other.gameObject.tag == "SmallStarTag") {
+			this.score += 10;
+		} else if (other.gameObject.tag == "LargeStarTag") {
+			this.score += 20;
+		} else if (other.gameObject.tag == "SmallCloudTag") {
+			this.score += 15;
+		} else if (other.gameObject.tag == "LargeCloudTag") {
+			this.score += 30;
+		}
+			
+	}
+}
